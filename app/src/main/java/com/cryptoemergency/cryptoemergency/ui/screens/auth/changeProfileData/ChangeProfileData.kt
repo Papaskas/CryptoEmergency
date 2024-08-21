9c3373f3be24c1711141037c157e8268ddb71123
pavel <pavel.glugxix@gmail.com>
2024-08-21 17:37:16 +0000

    Добавлена затычка добавления фотографии в редактирование профиля

--- app/src/main/java/com/cryptoemergency/cryptoemergency/ui/screens/auth/changeProfileData/ChangeProfileData.kt
+++ app/src/main/java/com/cryptoemergency/cryptoemergency/ui/screens/auth/changeProfileData/ChangeProfileData.kt
@@ -1,11 +1,18 @@
 package com.cryptoemergency.cryptoemergency.ui.screens.auth.changeProfileData
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.cryptoemergency.cryptoemergency.R
 import com.cryptoemergency.cryptoemergency.ui.common.ScrollableScreen
 import com.cryptoemergency.cryptoemergency.ui.common.inputs.Input
 
@@ -14,6 +21,12 @@ fun ChangeProfileDataScreen(
     viewModel: ChangeProfileDataViewModel = hiltViewModel()
 ) {
     ScrollableScreen {
+        Spacer(Modifier.height(20.dp))
+
+        EditAvatar(Modifier.align(Alignment.CenterHorizontally))
+
+        Spacer(Modifier.height(30.dp))
+
         Input(
             label = "Ваше имя",
             value = viewModel.firstName,
@@ -42,6 +55,25 @@ fun ChangeProfileDataScreen(
         Input(
             label = "Язык",
             value = viewModel.language,
+        )
+    }
+}
+
+@Composable
+private fun EditAvatar(modifier: Modifier) {
+    Box(modifier) {
+        Image(
+            painter = painterResource(R.drawable.avatar_placeholder2),
+            contentDescription = null,
+            modifier = Modifier.size(90.dp),
+        )
+        Image(
+            painter = painterResource(R.drawable.btn_edit),
+            contentDescription = null,
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .offset(x = 7.dp, y = 10.dp)
+            ,
         )
     }
 }
